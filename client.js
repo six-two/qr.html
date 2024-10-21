3fc180ba661a7dc21339f24c1c2d1b1380f15eed
Maggie Lagos <maggie-lagos@users.noreply.github.com>
2024-10-21 19:30:20 -0400

    Download and copy to clipboard buttons (#7)
    * add download and copy to clipboard buttons
    
    * add pears
    
    ---------
    
    Co-authored-by: Maggie Lagos <maggiejlagos+code@gmail.com>

--- client.js
+++ client.js
@@ -1,43 +1,29 @@
 console.log("QR code generator for PCM by github.com/maggie-lagos")
 
-const get_or_default_int = (name, default_value) => {
-    const stored = localStorage.getItem(name);
-    if (stored != null) {
-        try {
-            return parseInt(stored);
-        } catch (error) {
-            console.warn(`Failed to parse stored entry for '${name}'`);
-        } 
-    }
-    return default_value;
-}
-
 const input_textarea = document.getElementById("input");
 const result_qr = document.getElementById("result-qr");
 const result_msg = document.getElementById("result-msg");
+const download_btn = document.getElementById("download");
+const copy_btn = document.getElementById("copy-to-clip");
 
-
-window.addEventListener("load", () => {
-    result_msg.style.display = "none";
-})
-
-const updateResultMsg = (message, isErr) => {
+const showError = (message) => {
     result_qr.innerHTML = '';
     
     result_msg.innerHTML = `${message}`;
-    result_msg.className = isErr ? "error": "qr-message";
+    result_msg.className = "error"
     result_msg.style.display = "block";    
 }
+
+var currentQrCode
 
 const generateQR = () => {
     const text = input_textarea.value;
     if (!text) {
-        updateResultMsg("Please type text first to generate your QR code.", true);
+        showError("Please type text first to generate your QR code.");
         return
     }
 
     const qrSize = result_qr.offsetWidth/2;
-
     try {
         qrCode = new QRCodeStyling({
             width: qrSize,
@@ -54,16 +40,52 @@ const generateQR = () => {
                 margin: 0
             },
         });
-        //console.log(qrCode)
 
-        updateResultMsg("Use Camera App to scan", false)   // also clears previous QR code
+        result_qr.innerHTML = '';
+        result_msg.style.display = "none";
+
         qrCode.append(result_qr);
+        currentQrCode = qrCode;
     } catch (error) {
-        updateResultMsg("Failed to generate the QR code! Please try a different input.", true);
+        console.log(error)
+        showError("Failed to generate the QR code! Please try a different input.");
     }
 }
 
-// Update the QR code whenever the text changes
+const downloadQR = () => {
+    if (!currentQrCode){
+        showError("Please type text first to generate your QR code.");
+    }
+
+    currentQrCode.getRawData("png").then((pngBlob) => {
+        let blobUrl = URL.createObjectURL(pngBlob);
+
+        let link = document.createElement('a');
+        link.download = 'pcm-qr.png';
+        link.href = blobUrl
+        link.click();
+    });
+}
+
+const copyQR = () => {
+    if (!currentQrCode){
+        showError("Please type text first to generate your QR code.");
+    }
+
+    currentQrCode.getRawData("png").then((pngBlob) => {
+        let item = new ClipboardItem({ "image/png": pngBlob });
+        navigator.clipboard.write([item]); 
+    });
+}
+
+// Remove initial no-JS warning
+window.addEventListener("load", () => {
+    result_msg.style.display = "none";
+})
+
 input_textarea.addEventListener("input", generateQR);
-window.addEventListener("resize", generateQR);
+window.addEventListener("resize", generateQR);  // resize here because QR size is set as px by qr library, not CSS
+
+download_btn.addEventListener("click", downloadQR);
+copy_btn.addEventListener("click", copyQR);
 
